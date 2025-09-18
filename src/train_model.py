866891f9fb04f60f61a50a76e8df0a1d38cc8af0
Maxim <nedelcomaxim5@gmail.com>
2025-09-18 13:37:14 +0300

    Verify dataset and import libraries

---
+++ src/train_model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import joblib
+
+df = pd.read_csv("data/IMLP4_TASK_03-products.csv")
+print(df.head())
+print(df.info())
